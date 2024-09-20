212a3621a78521d402dd798c49b4cfccc510ec8f
Purna Shrestha <crush9918@gmail.com>
2024-09-20 23:25:34 +0545

    Feat: Create footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/ui/Navbar";
 import About from "./components/About";
 
 import img1 from "./assets/img1.jpg";
+import Footer from "./components/ui/Footer";
 
 const App: React.FC = () => {
   return (
@@ -11,11 +12,11 @@ const App: React.FC = () => {
       {/* <Display /> */}
       <Navbar />
       <section className="h-screen bg-gradient-to-r from-dark/80 via-dark/30 to-dark/80">
-      <img src={img1} alt="" className="size-full object-contain bg-black" />
+        <img src={img1} alt="" className="size-full object-contain bg-black" />
       </section>
       <About />
 
-      <section className="h-screen"></section>
+      <Footer />
     </>
   );
 };
