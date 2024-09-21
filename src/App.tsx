006d49ed1a4f21b5ae00896f95da3eeb9207b221
Purna Shrestha <crush9918@gmail.com>
2024-09-21 23:15:15 +0545

    feat: Update instagram wall and also fix responsive on mobile on hero video

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import About from "./components/About";
 import img1 from "./assets/img1.jpg";
 import Footer from "./components/ui/Footer";
 import InstagramWall from "./components/ui/InstagramWall";
-import VideoPlayer from "./components/VideoPlayer";
+// import VideoPlayer from "./components/VideoPlayer";
 
 // import makuraVideo from "./assets/video/makura.mp4";
 import makuraVideo2 from "./assets/video/makuraa2.mp4";
@@ -22,13 +22,17 @@ const App: React.FC = () => {
         <DisplayVideo
           poster={img1}
           video={makuraVideo2}
-          videoClassName="w-full h-80 sm:h-[50vh] sm:min-h-96 lg:h-[36rem] xl:h-[120vh] 2xl:h-[115vh] object-cover z-10"
+          videoClassName="w-full h-full sm:h-[50vh] sm:min-h-96 lg:h-[36rem] xl:h-[120vh] 2xl:h-[115vh] object-cover z-10"
         />
-        <div className="bg-gradient-to-t from-black/60 to-black/0 absolute inset-0 size-full z-0 flex items-center justify-end flex-col text-light pb-12 2xl:pb-12 space-y-6">
-          <span className="text-base uppercase opacity-90">Makura</span>
-          <h2 className="text-5xl text-light">Wear the Spirit with Makura</h2>
-          <div className="w-px h-28 bg-light/50 bottom-0"></div>
-          <button className="group relative text-light hover:text-white transition-all duration-500 hover:scale-110">
+        <div className="bg-gradient-to-t from-black/60 to-black/0 absolute inset-0 size-full z-0 flex items-center justify-end flex-col text-light pb-12 2xl:pb-12 space-y-3 md:space-y-6">
+          <span className="text-sm md:text-base uppercase opacity-90">
+            Makura
+          </span>
+          <h2 className="text-xl md:text-5xl text-light mx-8 md:mx-0 leading-relaxed shadow text-center">
+            Wear the Spirit with Makura
+          </h2>
+          <div className="w-px h-10 md:h-28 bg-light/50 bottom-0"></div>
+          <button className="group relative text-light text-sm md:text-base hover:text-white transition-all duration-500 hover:scale-110">
             <span className="absolute -bottom-px -right-0 w-full h-px bg-light/60 scale-x-0 hover:scale-x-0 group-hover:scale-x-100 origin-center transition-all duration-300 ease-linear"></span>
             Explore Products
           </button>
--- src/components/ui/InstagramWall.tsx
+++ src/components/ui/InstagramWall.tsx
@@ -2,10 +2,19 @@ import React, { useEffect } from "react";
 
 const InstagramWall: React.FC = () => {
   const instagramPosts = [
+    "https://www.instagram.com/p/CqiCNmkJ2mc/?utm_source=ig_embed&amp;utm_campaign=loadingd",
+    "https://www.instagram.com/p/CaZgesIIIZs/?utm_source=ig_embed&amp;utm_campaign=loading",
+    "https://www.instagram.com/p/C9zojtHyvmQ/?utm_source=ig_embed&amp;utm_campaign=loading",
+    "https://www.instagram.com/p/Cs_VnfNyVC3/?utm_source=ig_embed&amp;utm_campaign=loading",
+    "https://www.instagram.com/reel/C1a6hU2P_Y1/?utm_source=ig_embed&amp;utm_campaign=loading",
     "https://www.instagram.com/reel/CozJr8jqHX1/?utm_source=ig_embed&utm_campaign=loading",
-    "https://www.instagram.com/p/CozJr8jqHX1/?utm_source=ig_embed",
-    "https://www.instagram.com/reel/CozJr8jqHX1/?utm_source=ig_embed",
-    "https://www.instagram.com/reel/CozJr8jqHX1/?utm_source=ig_embed",
+    "https://www.instagram.com/p/CyivN5pCo9R/?hl=en",
+    "https://instagram.com/p/CqiD_fPJ8bo/?hl=en&img_index=1",
+    "https://www.instagram.com/p/Co9tvBpoDAC/?hl=en",
+    "https://www.instagram.com/p/CmF5scOJTTD/?hl=en&img_index=1",
+    "https://www.instagram.com/p/Cim6b6kpmpF/?hl=en&img_index=1",
+    "https://www.instagram.com/p/CWafXkysfeU/?hl=en&img_index=1",
+    "https://www.instagram.com/p/CWc3rStP-uO/?hl=en",
   ];
 
   useEffect(() => {
@@ -17,21 +26,22 @@ const InstagramWall: React.FC = () => {
   }, []);
 
   return (
-    <main className="bg-dark text-light pb-0">
+    <main className="bg-light text-dark">
       <div className="flex items-center justify-center gap-4 flex-col max-w-xl mx-auto">
         <h3 className="text-xl sm:text-2xl md:text-3xl leading-snug lg:leading-snug uppercase">
           Instagram Wall
         </h3>
-        <p className="text-sm text-center text-light/60">
+        <p className="text-sm text-center text-dark/60">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cum
           itaque nulla iste rem tempora, sequi ducimus a sit dicta, ullam minus
           vero. Similique magni nihil, quia provident soluta illo harum
           cupiditate, excepturi totam nobis sunt assumenda. Velit!
         </p>
       </div>
-      <div className="mt-12 flex items-center justify-between text-light">
+      <div className="scroll mt-20 flex gap-6 text-light max-w-full overflow-x-auto">
         {instagramPosts.map((postUrl, index) => (
-          <div key={index} className="">
+          <div key={index} className="mb-10">
+            {/* mb is for scrollbar */}
             <blockquote
               className="instagram-media"
               data-instgrm-permalink={postUrl}
@@ -43,6 +53,20 @@ const InstagramWall: React.FC = () => {
             </blockquote>
           </div>
         ))}
+      </div>
+
+      {/* <div className="group flex items-center justify-end flex-col text-dark pb-12 2xl:pb-12 space-y-6 -z-10 transition-all duration-500">
+        <div className="w-px h-28 group-hover:h-16 bg-dark/50 bottom-0 transition-all duration-500"></div>
+        <button className="group relative transition-all duration-500 hover:scale-110 font-semibold">
+          <span className="absolute -bottom-px -right-0 w-full h-px bg-dark/60 scale-x-100 group-hover:scale-x-0 origin-center transition-all duration-300 ease-linear"></span>
+          View more on Instagram
+        </button>
+      </div> */}
+      <div className=" mt-10 flex items-center justify-center">
+        <button className="group relative transition-all duration-500 hover:scale-110 font-semibold text-center">
+          <span className="absolute -bottom-px -right-0 w-full h-px bg-dark/60 scale-x-100 group-hover:scale-x-0 origin-center transition-all duration-300 ease-linear"></span>
+          View more on Instagram
+        </button>
       </div>
     </main>
   );
